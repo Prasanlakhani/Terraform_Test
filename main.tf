0af0cfd009e04207890ee879899cb729d1180b84
prasanlakhani <prasanlakhani@gmail.com>
2022-08-23 13:56:22 +0530

    Update

--- main.tf
+++ main.tf
@@ -38,43 +38,50 @@ module "vpc" {
   project_name = var.project_name
 }
 
-module "subnetwork" {
-  source      = "./Module/subnet"
-  for_each    = var.subnetworks
-  subnet_name = "subnet-${each.value.region_name}"
-  #subnet_name = each.value.subnet_name
-  ipaddress   = each.value.ipaddress
-  region_name = each.value.region_name
-  #vpc_network_name = google_compute_network.vpc_network.name
-  #vpc_network_name =  module.vpc.name
-  vpc_network_name = var.vpc_name
+#module "subnetwork" {
+#  source      = "./Module/subnet"
+#  for_each    = var.subnetworks
+#  subnet_name = "subnet-${each.value.region_name}"
+#  #subnet_name = each.value.subnet_name
+#  ipaddress   = each.value.ipaddress
+#  region_name = each.value.region_name
+#  #vpc_network_name = google_compute_network.vpc_network.name
+#  #vpc_network_name =  module.vpc.name
+#  vpc_network_name = var.vpc_name
 
-}
+#}
 
-module "router_network" {
-  source           = "./Module/router"
-  for_each         = var.subnetworks
-  router_name      = "router-${each.value.region_name}"
-  region_name      = each.value.region_name
-  vpc_network_name = var.vpc_name
+#module "router_network" {
+#  source           = "./Module/router"
+#  for_each         = var.subnetworks
+#  router_name      = "router-${each.value.region_name}"
+#  region_name      = each.value.region_name
+#  vpc_network_name = var.vpc_name
 
-}
+#}
 
-module "nat" {
-  source          = "./Module/nat"
-  for_each        = var.subnetworks
-  nat_name        = "nat-${each.value.region_name}"
-  nat_router_name = "router-${each.value.region_name}"
-  nat_region_name = each.value.region_name
-  #nat_router_name = module.router_network.router_name
-  #nat_region_name = module.router_network.region_name
-  #region_name = each.value.region_name
-  #vpc_network_name = var.vpc_name
+#module "nat" {
+#  source          = "./Module/nat"
+#  for_each        = var.subnetworks
+#  nat_name        = "nat-${each.value.region_name}"
+#  nat_router_name = "router-${each.value.region_name}"
+#  nat_region_name = each.value.region_name
+#  #nat_router_name = module.router_network.router_name
+#  #nat_region_name = module.router_network.region_name
+#  #region_name = each.value.region_name
+#  #vpc_network_name = var.vpc_name
 
 
 
-}
+#}
 
+#output "network_id" {
+#  value = module.vpc.self_link
+#}
+
+#output "subnetworks" {
+#  value = module.subnetwork[*].name
+#}
 
 
 
