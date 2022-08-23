553d396587cfad73460da6b2b6579a04b3e2d038
prasanlakhani <prasanlakhani@gmail.com>
2022-08-23 17:48:07 +0530

    Update

---
+++ Module/subnet/output.tf
@@ -0,0 +1,3 @@
+output "subnet_out" {
+  value = google_compute_subnetwork.subnet
+}
--- main.tf
+++ main.tf
@@ -38,18 +38,19 @@ module "vpc" {
   project_name = var.project_name
 }
 
-#module "subnetwork" {
-#  source      = "./Module/subnet"
-#  for_each    = var.subnetworks
-#  subnet_name = "subnet-${each.value.region_name}"
-#  #subnet_name = each.value.subnet_name
-#  ipaddress   = each.value.ipaddress
-#  region_name = each.value.region_name
-#  #vpc_network_name = google_compute_network.vpc_network.name
-#  #vpc_network_name =  module.vpc.name
-#  vpc_network_name = var.vpc_name
+module "subnetwork" {
+  source      = "./Module/subnet"
+  for_each    = var.subnetworks
+  subnet_name = "subnet-${each.value.region_name}"
+  #subnet_name = each.value.subnet_name
+  ipaddress   = each.value.ipaddress
+  region_name = each.value.region_name
+  #vpc_network_name = google_compute_network.vpc_network.name
+  #vpc_network_name =  module.vpc.name
+  vpc_network_name = module.vpc.vpc_out
 
-#}
+}
+
 
 #module "router_network" {
 #  source           = "./Module/router"
